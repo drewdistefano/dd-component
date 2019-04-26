3e382a1a06b7dd9acc8386ef23c5c0cd5a4b0078
Drew <drew.distefano@me.com>
2019-04-26 12:22:47 -0500

    fixed bug: wasn't exporting pool

--- db/SDC/MariaDB-config.js
+++ db/SDC/MariaDB-config.js
@@ -1,14 +1,15 @@
 const mariadb = require('mariadb');
+console.log('creating pool...')
+console.log('MARIA_HOST>>>', process.env.MARIA_HOST)
 const pool = mariadb.createPool({
   host: process.env.MARIA_HOST,
   user: 'app',
   password: process.env.MARIA_PASSWORD,
   database: 'component',
+  port: process.env.MARIA_PORT
   // connectionLimit = 200
 });
 
-// //attempt to export connection
-// pool.getConnection()
-// .then((connection)=>{ module.exports = { connection }})
+let conn = pool.getConnection;
 
 module.exports = { pool };
