7b96d1a841ad14cc1d7aa81dc322a7d84fb549f6
Drew <drew.distefano@me.com>
2019-04-29 16:19:11 -0500

    set up mariadb with callbacks

--- db/SDC/MariaDB-config.js
+++ db/SDC/MariaDB-config.js
@@ -1,7 +1,5 @@
-const mariadb = require('mariadb');
-console.log('creating pool...')
-
-const pool = mariadb.createPool({
+const mariadb = require('mariadb/callback');
+const conn = mariadb.createConnection({
   host: process.env.MARIA_HOST,
   user: 'app',
   password: process.env.MARIA_PASSWORD,
@@ -9,6 +7,27 @@ const pool = mariadb.createPool({
   port: process.env.MARIA_PORT
 });
 
-let conn = pool.getConnection;
+conn.connect((err)=>{
+  if (err){
+    console.log('Unable to connect to MariaDB')
+  }
+  else {
+    console.log('Connected to MariaDB!')
+  }
+})
 
-module.exports = { pool };
+module.exports = { conn };
+
+//USING A POOL/PROMISES
+// const mariadb = require('mariadb');
+// console.log('creating pool...')
+
+// const pool = mariadb.createPool({
+//   host: process.env.MARIA_HOST,
+//   user: 'app',
+//   password: process.env.MARIA_PASSWORD,
+//   database: 'component',
+//   port: process.env.MARIA_PORT
+// });
+
+// module.exports = { pool };
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const env = require('dotenv').config();
 const app = require('./10M-server-app.js')
 const port = process.env.port || 4000;
 
+
 app.listen(port, (err, success) => {
   if (err) console.log('Server run error')
   else console.log(`Server running on port: ${port}`)
